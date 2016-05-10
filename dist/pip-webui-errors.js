1f9251859b46e2c073b578145f654a9b192f5fbe
Anastas Fonotov <anastasf@gmail.com>
2016-05-10 14:49:52 +0300

    update

--- dist/pip-webui-errors.js
+++ dist/pip-webui-errors.js
@@ -1,18 +1,3 @@
-/**
- * @file Registration of all error handling components
- * @copyright Digital Living Software Corp. 2014-2016
- */
-
-/* global angular */
-
-(function () {
-    'use strict';
-
-    angular.module('pipErrorHandling', [
-        'pipErrors.Pages'
-    ]);
-    
-})();
 (function(module) {
 try {
   module = angular.module('pipErrors.Templates');
@@ -234,6 +219,21 @@ module.run(['$templateCache', function($templateCache) {
 }]);
 })();
 
+/**
+ * @file Registration of all error handling components
+ * @copyright Digital Living Software Corp. 2014-2016
+ */
+
+/* global angular */
+
+(function () {
+    'use strict';
+
+    angular.module('pipErrorHandling', [
+        'pipErrors.Pages'
+    ]);
+    
+})();
 /* global angular */
 
 (function () {
