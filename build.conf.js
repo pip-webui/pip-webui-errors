fd5472ec929ce2cc5266d6d35a949e4145b6f837
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-30 22:15:13 -0500

    Rebuilt

--- build.conf.js
+++ build.conf.js
@@ -2,7 +2,7 @@ module.exports = {
     module: {
         name: 'pipErrors',
         styles: 'errors',
-        export: 'pip',
+        export: 'pip.errors',
         standalone: 'pip.errors'
     },
     build: {
