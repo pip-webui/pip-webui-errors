e169b99db3cb4c86a3f572864c731260c98040e5
Anastas Fonotov <anastasf@gmail.com>
2017-03-24 20:33:23 +0200

    'rebuild'

--- dist/pip-webui-errors.d.ts
+++ dist/pip-webui-errors.d.ts
@@ -1,5 +1,6 @@
 declare module pip.errors {
 
+
 export class ErrorPageConfig {
     Active: boolean;
     Name: string;
@@ -39,8 +40,6 @@ export interface IErrorPageConfigProvider extends ng.IServiceProvider {
 
 
 
-
-
 export interface IFormErrorsService {
     errorsWithHint(field: any): any;
     touchedErrorsWithHint(form: ng.IFormController, field: any): any;
@@ -50,18 +49,19 @@ export interface IFormErrorsService {
     goToUnhandledErrorPage(error: any): any;
 }
 
+
 export let ErrorsMaintenanceState: string;
 export let MaintenanceErrorEvent: string;
 
 export let ErrorsMissingRouteState: string;
 export let StateNotFoundEvent: string;
 
+
 export let ErrorsConnectionState: string;
 export let ErrorsConnectionEvent: string;
 
 export let ErrorsUnknownState: string;
 export let ErrorsUnknownEvent: string;
-
 
 
 }
