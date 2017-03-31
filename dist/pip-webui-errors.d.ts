3b1e872d62b6f3f988608932627e18cadb365fb6
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-31 11:46:55 -0700

    'Updated version to 2.0.2'

--- dist/pip-webui-errors.d.ts
+++ dist/pip-webui-errors.d.ts
@@ -1,6 +1,5 @@
 declare module pip.errors {
 
-
 export class ErrorPageConfig {
     Active: boolean;
     Name: string;
@@ -40,6 +39,7 @@ export interface IErrorPageConfigProvider extends ng.IServiceProvider {
 
 
 
+
 export interface IFormErrorsService {
     errorsWithHint(field: any): any;
     touchedErrorsWithHint(form: ng.IFormController, field: any): any;
@@ -50,18 +50,18 @@ export interface IFormErrorsService {
 }
 
 
-export let ErrorsMaintenanceState: string;
-export let MaintenanceErrorEvent: string;
-
 export let ErrorsMissingRouteState: string;
 export let StateNotFoundEvent: string;
+
+export let ErrorsMaintenanceState: string;
+export let MaintenanceErrorEvent: string;
 
 export let ErrorsConnectionState: string;
 export let ErrorsConnectionEvent: string;
 
-
 export let ErrorsUnknownState: string;
 export let ErrorsUnknownEvent: string;
+
 
 
 }
