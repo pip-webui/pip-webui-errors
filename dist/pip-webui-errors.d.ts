11d23d31f53699bf51feaf26fd78435055b59246
Anastas Fonotov <anastasf@gmail.com>
2017-09-06 15:59:47 +0300

    'rebuild'

--- dist/pip-webui-errors.d.ts
+++ dist/pip-webui-errors.d.ts
@@ -1,17 +1,6 @@
 declare module pip.errors {
 
 
-
-
-export interface IFormErrorsService {
-    errorsWithHint(field: any): any;
-    touchedErrorsWithHint(form: ng.IFormController, field: any): any;
-    resetFormErrors(form: ng.IFormController, errors?: boolean): void;
-    resetFieldsErrors(form: ng.IFormController, field: any): void;
-    setFormError(form: ng.IFormController, error: any, errorFieldMap: any): void;
-    goToUnhandledErrorPage(error: any): any;
-}
-
 export class ErrorPageConfig {
     Active: boolean;
     Name: string;
@@ -50,6 +39,17 @@ export interface IErrorPageConfigProvider extends ng.IServiceProvider {
     configs: ErrorPageConfigs;
 }
 
+
+
+export interface IFormErrorsService {
+    errorsWithHint(field: any): any;
+    touchedErrorsWithHint(form: ng.IFormController, field: any): any;
+    resetFormErrors(form: ng.IFormController, errors?: boolean): void;
+    resetFieldsErrors(form: ng.IFormController, field: any): void;
+    setFormError(form: ng.IFormController, error: any, errorFieldMap: any): void;
+    goToUnhandledErrorPage(error: any): any;
+}
+
 class HttpResponseInterceptor implements ng.IHttpInterceptor {
     private $q;
     private $location;
@@ -69,11 +69,11 @@ export let ErrorsConnectionState: string;
 export let ErrorsConnectionEvent: string;
 
 
-export let ErrorsUnknownState: string;
-export let ErrorsUnknownEvent: string;
-
 export let ErrorsUnsupportedState: string;
 export let ErrorsUnsupportedEvent: string;
+
+export let ErrorsUnknownState: string;
+export let ErrorsUnknownEvent: string;
 
 }
 
