9a6a1b5d6e8b10cb1e0fe009c5afd118fc1ed173
Anastas Fonotov <anastasf@gmail.com>
2017-11-06 11:44:24 +0300

    'rebuild fix session'

--- dist/pip-webui-errors.d.ts
+++ dist/pip-webui-errors.d.ts
@@ -69,9 +69,9 @@ export let StateNotFoundEvent: string;
 export let ErrorsConnectionState: string;
 export let ErrorsConnectionEvent: string;
 
-
 export let ErrorsUnknownState: string;
 export let ErrorsUnknownEvent: string;
+
 
 export let ErrorsUnsupportedState: string;
 export let ErrorsUnsupportedEvent: string;
